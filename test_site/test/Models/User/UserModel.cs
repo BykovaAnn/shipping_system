8f9090414cdf957240354104f750f218b58e37da
BykovaAnn <gikovka@gmail.com>
2015-10-30 21:43:54 +0200

    Test_site
    Должно залить то, что показывала Кристина

---
+++ test_site/test/Models/User/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Models.User
+{
+    public class UserModel
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}

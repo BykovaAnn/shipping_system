851af48696d8dd0086e52c0f175c97f9590f5bc0
BykovaAnn <gikovka@gmail.com>
2015-11-23 09:05:42 +0200

    Проект с membership
    + обновление для бд

---
+++ Shipping_system/Shipping_system/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shipping_system.Models;
+using WebMatrix.WebData;
+using System.Web.Security;
+
+namespace Shipping_system.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login logindata, string ReturnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (WebSecurity.Login(logindata.Username, logindata.Password))
+                {
+                    if (ReturnUrl != null)
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Sorry, smth wrong!");
+            return View(logindata);
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register registerdata, string role)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    WebSecurity.CreateUserAndAccount(registerdata.Username, registerdata.Password);
+                    Roles.AddUserToRole(registerdata.Username, role);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (System.Web.Security.MembershipCreateUserException ex)
+                {
+                    ModelState.AddModelError("", "Sorry, smth exist!");
+                    return View(registerdata);
+                }
+            }
+            ModelState.AddModelError("", "Sorry, smth exist!");
+            return View(registerdata);
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ update_db_membership.sql
@@ -0,0 +1,107 @@
+USE [master]
+GO
+
+/****** Object:  Database [shipping_system]    Script Date: 23.11.2015 8:57:03 ******/
+DROP DATABASE [shipping_system]
+GO
+
+/****** Object:  Database [shipping_system]    Script Date: 23.11.2015 8:57:03 ******/
+CREATE DATABASE [shipping_system] ON  PRIMARY 
+( NAME = N'shipping_system', FILENAME = N'D:\shipping_system.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'shipping_system_log', FILENAME = N'D:\shipping_system_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [shipping_system] SET COMPATIBILITY_LEVEL = 100
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [shipping_system].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [shipping_system] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET AUTO_CLOSE ON 
+GO
+
+ALTER DATABASE [shipping_system] SET AUTO_CREATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [shipping_system] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [shipping_system] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [shipping_system] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [shipping_system] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [shipping_system] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [shipping_system] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [shipping_system] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [shipping_system] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [shipping_system] SET  READ_WRITE 
+GO
+
